49c5861702830973fdbceaf3f1cd468c3718ef8c
bofmar <bofmar@outlook.com>
2020-10-23 17:31:19 +0300

    added create_displayed_word method in game.rb

--- lib/hangman/game.rb
+++ lib/hangman/game.rb
@@ -3,28 +3,36 @@
 module Hangman
   class Game
 
-    attr_accessor :secret_word
+    attr_accessor :secret_word, :disp_word, :board
 
     def initialize
       @secret_word = choose_word
+      @disp_word = create_displayed_word
+
+      @board = Board.new
     end
 
     def choose_word
       contents = File.read("./lib/dictionary.txt")
-      rng = Random.new
-      rng = rng.rand(0..61405)
-      
       contents = contents.split("\n")
+      
+      rng = Random.new
+      rng = rng.rand(0..contents.length - 1)
+      
 
       word = contents[rng]
-
-
 
       if word.length > 5 && word.length < 10
         return word.upcase
       else
         choose_word
       end
+    end
+
+    def create_displayed_word
+      displayed_word = secret_word.split("")
+      displayed_word = displayed_word.map { |el| el = "_"}
+      return displayed_word
     end
 
   end
