8755b102a0d41b5669bf18cadb9352d6b13503d8
bofmar <bofmar@outlook.com>
2020-10-23 15:51:56 +0300

    added update_choices method and tests in board.rb

--- lib/hangman/board.rb
+++ lib/hangman/board.rb
@@ -3,18 +3,21 @@
 module Hangman
   class Board
 
-    attr_accessor :number_of_incorect_guesses, :incorect_letters
-    def initialize incorect_letters = []
-      @incorect_letters = incorect_letters
-      @number_of_incorect_guesses = @incorect_letters.length
+    attr_accessor :number_of_incorrect_guesses, :incorrect_letters, :choices, :correct_letters
+    def initialize incorrect_letters = [], correct_letters = []
+      @incorrect_letters = incorrect_letters
+      @number_of_incorrect_guesses = @incorrect_letters.length
+      @correct_letters = correct_letters
+      @choices = ("A".."Z").to_a
+      @choices = update_choices
     end
 
-    def draw_self incorect, word, choices
+    def draw_self incorrect, word, choices
       
     end
 
     def update_choices
-      
+      new_choices = choices.select { |char| incorrect_letters.include?(char) ? false : correct_letters.include?(char)? false : true }
     end
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,15 +5,30 @@ module Hangman
   RSpec.describe Board do
     
     context "#initialize" do
-      it "can be initialized without any parameters and get an empty array for incorect_letters and a 0 for number_of_incorect_guesses" do
+      it "can be initialized without any parameters and get an empty array for incorrect_letters and a 0 for number_of_incorrect_guesses. correct_letters should be returning an empty array" do
         b = Board.new
-        expect(b.number_of_incorect_guesses).to eq 0
-        expect(b.incorect_letters.length).to eq 0
+        expect(b.number_of_incorrect_guesses).to eq 0
+        expect(b.incorrect_letters.length).to eq 0
+        expect(b.correct_letters.length).to eq 0
       end
-      it "can be passed a parameter for incorect_letters and return return the correct values" do
+      it "can be passed one parameter for incorrect_letters and return return the correct values" do
         b = Board.new(["A","B","C"])
-        expect(b.number_of_incorect_guesses).to eq 3
-        expect(b.incorect_letters).to eq ["A","B","C"]
+        expect(b.number_of_incorrect_guesses).to eq 3
+        expect(b.incorrect_letters).to eq ["A","B","C"]
+        expect(b.correct_letters).to eq []
+      end
+      it "by default initializes the choices array with every uppercase letter" do
+        b = Board.new
+        test_array = ("A".."Z").to_a
+        expect(b.choices).to eq test_array
+      end
+      it "can be initialized with both correct and incorrect letters and return all values correctly" do
+        b = Board.new(["A","B","C"], ["D","E","F"])
+        expect(b.number_of_incorrect_guesses).to eq 3
+        expect(b.incorrect_letters).to eq ["A","B","C"]
+        expect(b.correct_letters).to eq ["D","E","F"]
+        test_array = ("G".."Z").to_a
+        expect(b.choices).to eq test_array
       end
     end
 
