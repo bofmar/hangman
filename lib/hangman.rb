0028b800fef9fa38ba7d55df21099306eb76fd3b
bofmar <bofmar@outlook.com>
2020-10-23 15:10:14 +0300

    added board and game files along with coresponding spec files

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,30 @@
 require "hangman/version"
 
+puts "test"
+
 module Hangman
   class Error < StandardError; end
   # Your code goes here...
 end
+
+# Odin Commandments
+# When game is initialized, look into the dictionary and choose a random word between 5 and 10 characters long
+# Draw the hang ,the stick figure and the rest of the UI like so :
+#
+#
+# -------
+# |/    |
+# |     0
+# |    /|\
+# |     |
+# |    / \
+# |
+# Incorect guesses : A,C,X (letters are colored red)
+# The word : _ R O G R A _ _ I N G
+# Game Over
+#
+#
+# Display which correct letters have already been chosen (and their position in the word, e.g. _ r o g r a _ _ i n g)
+# Every turn, allow the player to make a guess of a letter. It should be case insensitive. Update the display to reflect whether the letter was correct or incorrect. # If out of guesses, the player should lose.
+# Now implement the functionality where, at the start of any turn, instead of making a guess the player should also have the option to save the game.
+# When the program first loads, add in an option that allows you to open one of your saved games, which should jump you exactly back to where you were when you saved
