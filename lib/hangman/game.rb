280039ac0bae32063698a5ba3275d765c84c4e4b
bofmar <bofmar@outlook.com>
2020-10-23 17:51:09 +0300

    added checks for game over conditions and for the chosen letter

--- lib/hangman/game.rb
+++ lib/hangman/game.rb
@@ -3,12 +3,13 @@
 module Hangman
   class Game
 
-    attr_accessor :secret_word, :disp_word, :board
+    attr_accessor :secret_word, :disp_word, :board, :incorrect_letters, :correct_letters
 
     def initialize
       @secret_word = choose_word
       @disp_word = create_displayed_word
-
+      @incorrect_letters = []
+      @correct_letters = []
       @board = Board.new
     end
 
@@ -33,6 +34,37 @@ module Hangman
       displayed_word = secret_word.split("")
       displayed_word = displayed_word.map { |el| el = "_"}
       return displayed_word
+    end
+
+    def check_choice choice
+      if secret_word.include? choice
+        correct_letters << choice
+        board.correct_letters = correct_letters
+        update_disp_word(choice)
+      else
+        incorrect_letters << choice
+        board.incorrect_letters = incorrect_letters
+      end
+      if loss?
+        board.draw_self false, false, disp_word, secret_word
+      elsif win?
+        board.draw_self false, true ,disp_word, secret_word
+      else
+        board.draw_self true, false, disp_word, secret_word
+      end
+    end
+
+    def update_disp_word choice
+      index = secret_word.index(choice)
+      disp_word[index] = choice
+    end
+
+    def loss?
+      incorrect_letters.length == 6 ? true : false
+    end
+
+    def won?
+      secret_word == disp_word.join("") ? true : false
     end
 
   end
